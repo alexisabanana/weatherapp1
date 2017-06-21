21efccb076e077c3f10c5aad9ad77cf1e60db374
alex mcquire <alexander.mcquire@outlook.com>
2017-06-21 17:53:10 +0100

    weather app 0.0012
    a snippit for creating your own config

---
+++ config1.js
@@ -0,0 +1,5 @@
+const config = {
+  GoogleKey : 'YOUR KEYHERE', //addyour key here
+  METOFFCEKEY : 'YOURKEY HERE', //add your key here
+}
+ //$("#mapapi").html('https://maps.googleapis.com/maps/api/js?key='+GoogleKey+'&callback=initMap');
